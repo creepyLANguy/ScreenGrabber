c2a6e1bde1deefbe611e6c05643ef7631ee58a0b
Altamish Mahomed <gambit318@gmail.com>
2020-10-17 11:25:51 +0200

    Made config checker easier to read.

--- ScreenGrabber/ScreenGrabber/ConfigSanityChecker.hpp
+++ ScreenGrabber/ScreenGrabber/ConfigSanityChecker.hpp
@@ -2,40 +2,40 @@
 
 #include "ConfigVariables.h"
 
-#define WARN(Variable, CheckType) PrintWarning((#Variable), CheckType)
-#define WARNIF(Variable, CheckType) Check(Variable, CheckType, (#Variable))
+#define WARN(Variable, Is) PrintWarning((#Variable), Is)
+#define WARNIF(Variable, Is) Check(Variable, Is, (#Variable))
 
 
-enum class CheckType
+enum class Is
 {
-  NONE,
-  LESSTHANZERO,
-  LESSTHANOREQUALTOZERO,
-  EQUALTOZERO,
-  MORETHANOREQUALTOZERO,
-  MORETHANZERO,
-  LESSTHANMIN,
-  MORETHANMAX,
-  NOTONEORZERO,
-  NOTANOPTION,
+  UnimplementedCheck,
+  LessThanZero,
+  LessThanOrEqualToZero,
+  EqualToZero,
+  MoreThanOrEqualToZero,
+  MoreThanZero,
+  LessThanMin,
+  MoreThanMax,
+  NotOneOrZero,
+  NotAnOption,
 };
 
 
-std::map<CheckType, const char*> check_map = {
-  {CheckType::NONE, " - CHECK TYPE NOT IMPLEMENTED!"},
-  {CheckType::LESSTHANZERO, " is less than zero."},
-  {CheckType::LESSTHANOREQUALTOZERO,  " is less than or equal to zero."},
-  {CheckType::EQUALTOZERO,  " is equal to zero."},
-  {CheckType::MORETHANOREQUALTOZERO, " is more than or equal to zero."},
-  {CheckType::MORETHANZERO, " is more than zero."},
-  {CheckType::LESSTHANMIN, " is likely less than the minimum allowed value."},
-  {CheckType::MORETHANMAX,  " is likely more than the maximum allowed value."},
-  {CheckType::NOTONEORZERO,  " is not a 1 or 0, as is required for feature toggles."},
-  {CheckType::NOTANOPTION,  " is not the value of any corresponding options."}
+std::map<Is, const char*> check_map = {
+  {Is::UnimplementedCheck, " - CHECK TYPE NOT IMPLEMENTED!"},
+  {Is::LessThanZero, " is less than zero."},
+  {Is::LessThanOrEqualToZero,  " is less than or equal to zero."},
+  {Is::EqualToZero,  " is equal to zero."},
+  {Is::MoreThanOrEqualToZero, " is more than or equal to zero."},
+  {Is::MoreThanZero, " is more than zero."},
+  {Is::LessThanMin, " is likely less than the minimum allowed value."},
+  {Is::MoreThanMax,  " is likely more than the maximum allowed value."},
+  {Is::NotOneOrZero,  " is not a 1 or 0, as is required for feature toggles."},
+  {Is::NotAnOption,  " is not the value of any corresponding options."}
 };
 
 
-inline void PrintWarning(const char* variableName, const CheckType checkType)
+inline void PrintWarning(const char* variableName, const Is checkType)
 {
   const char* warning = check_map.find(checkType)->second;
   cout << "WARNING:\r\n" << variableName << warning << endl << endl;
@@ -43,27 +43,27 @@ inline void PrintWarning(const char* variableName, const CheckType checkType)
 
 
 template <class T>
-void Check(const T variable, const CheckType checkType, const char* variableName)
+void Check(const T variable, const Is checkType, const char* variableName)
 {
   switch (checkType)
   {
-  case CheckType::LESSTHANZERO:
+  case Is::LessThanZero:
     if (variable < 0) { PrintWarning(variableName, checkType); }
     break;
-  case CheckType::LESSTHANOREQUALTOZERO:
+  case Is::LessThanOrEqualToZero:
     if (variable <= 0) { PrintWarning(variableName, checkType); }
     break;
-  case CheckType::EQUALTOZERO:
+  case Is::EqualToZero:
     if (variable == 0) { PrintWarning(variableName, checkType); }
     break;
-  case CheckType::MORETHANOREQUALTOZERO:
+  case Is::MoreThanOrEqualToZero:
     if (variable >= 0) { PrintWarning(variableName, checkType); }
     break;
-  case CheckType::MORETHANZERO:
+  case Is::MoreThanZero:
     if (variable > 0) { PrintWarning(variableName, checkType); }
     break;
   default:
-    PrintWarning(variableName, CheckType::NONE);
+    PrintWarning(variableName, Is::UnimplementedCheck);
     break;
   }
 }
@@ -74,60 +74,66 @@ inline void CheckConfigValues_General()
   //AL.
   //TODO
   //Check led counts and offset
+  WARNIF(leds.LED_COUNT_LEFT, Is::LessThanOrEqualToZero);
+  WARNIF(leds.LED_COUNT_RIGHT, Is::LessThanOrEqualToZero);
+  WARNIF(leds.LED_COUNT_UPPER, Is::LessThanOrEqualToZero);
+  WARNIF(leds.LED_COUNT_LOWER, Is::LessThanOrEqualToZero);
 
-  WARNIF(width, CheckType::LESSTHANOREQUALTOZERO);
-  WARNIF(height, CheckType::LESSTHANOREQUALTOZERO);
+  WARNIF(originPositionOffset, Is::LessThanZero);
 
-  WARNIF(borderSamplePercentage, CheckType::LESSTHANOREQUALTOZERO);
-  if (borderSamplePercentage > 100) { WARN(borderSamplePercentage, CheckType::MORETHANMAX); }
+  WARNIF(width, Is::LessThanOrEqualToZero);
+  WARNIF(height, Is::LessThanOrEqualToZero);
 
-  WARNIF(lowerBufferPercentage, CheckType::LESSTHANZERO);
-  WARNIF(upperBufferPercentage, CheckType::LESSTHANZERO);
-  WARNIF(leftBufferPercentage, CheckType::LESSTHANZERO);
-  WARNIF(rightBufferPercentage, CheckType::LESSTHANZERO);
+  WARNIF(borderSamplePercentage, Is::LessThanOrEqualToZero);
+  if (borderSamplePercentage > 100) { WARN(borderSamplePercentage, Is::MoreThanMax); }
 
-  if (lowerBufferPercentage > (1.0 - borderSamplePercentage)) { WARN(lowerBuffer, CheckType::MORETHANMAX); }
-  if (upperBufferPercentage > (1.0 - borderSamplePercentage)) { WARN(upperBuffer, CheckType::MORETHANMAX); }
-  if (leftBufferPercentage > (1.0 - borderSamplePercentage)) { WARN(leftBuffer, CheckType::MORETHANMAX); }
-  if (rightBufferPercentage > (1.0 - borderSamplePercentage)) { WARN(rightBuffer, CheckType::MORETHANMAX); }
+  WARNIF(lowerBufferPercentage, Is::LessThanZero);
+  WARNIF(upperBufferPercentage, Is::LessThanZero);
+  WARNIF(leftBufferPercentage, Is::LessThanZero);
+  WARNIF(rightBufferPercentage, Is::LessThanZero);
 
-  WARNIF(downscaler, CheckType::LESSTHANOREQUALTOZERO);
+  if (lowerBufferPercentage > (1.0 - borderSamplePercentage)) { WARN(lowerBuffer, Is::MoreThanMax); }
+  if (upperBufferPercentage > (1.0 - borderSamplePercentage)) { WARN(upperBuffer, Is::MoreThanMax); }
+  if (leftBufferPercentage > (1.0 - borderSamplePercentage)) { WARN(leftBuffer, Is::MoreThanMax); }
+  if (rightBufferPercentage > (1.0 - borderSamplePercentage)) { WARN(rightBuffer, Is::MoreThanMax); }
 
-  WARNIF(sleepMS, CheckType::LESSTHANZERO);
-  WARNIF(chunkUpdateTimeoutMS, CheckType::LESSTHANZERO);
+  WARNIF(downscaler, Is::LessThanOrEqualToZero);
 
-  WARNIF(brightnessPercentage, CheckType::LESSTHANZERO);
-  if (brightnessPercentage > 100) { WARN(brightnessPercentage, CheckType::MORETHANMAX); }
+  WARNIF(sleepMS, Is::LessThanZero);
+  WARNIF(chunkUpdateTimeoutMS, Is::LessThanZero);
 
-  WARNIF(whiteDiffThresh, CheckType::LESSTHANZERO);
-  WARNIF(outlierDiffThresh, CheckType::LESSTHANZERO);
-  WARNIF(whiteLuminanceThresh, CheckType::LESSTHANZERO);
-  WARNIF(colourLuminanceThresh, CheckType::LESSTHANZERO);
+  WARNIF(brightnessPercentage, Is::LessThanZero);
+  if (brightnessPercentage > 100) { WARN(brightnessPercentage, Is::MoreThanMax); }
 
-  WARNIF(deltaEThresh, CheckType::LESSTHANZERO);
-  if (static_cast<int>(deltaEType) < 0) { WARN(deltaEType, CheckType::NOTANOPTION); }
+  WARNIF(whiteDiffThresh, Is::LessThanZero);
+  WARNIF(outlierDiffThresh, Is::LessThanZero);
+  WARNIF(whiteLuminanceThresh, Is::LessThanZero);
+  WARNIF(colourLuminanceThresh, Is::LessThanZero);
+
+  WARNIF(deltaEThresh, Is::LessThanZero);
+  if (static_cast<int>(deltaEType) < 0) { WARN(deltaEType, Is::NotAnOption); }
   //< 0 instead of stating SimpleRGBComparison cos it's not explicitly defined
   //in code and is executed when the deltae func is null.
 
-  if (captureType < CaptureType::PRIMARYDISPLAY) { WARN(deltaEType, CheckType::NOTANOPTION); }
+  if (captureType < CaptureType::PRIMARYDISPLAY) { WARN(deltaEType, Is::NotAnOption); }
 
-  WARNIF(animationSteps, CheckType::LESSTHANOREQUALTOZERO);
-  WARNIF(animationDelayMS, CheckType::LESSTHANZERO);
+  WARNIF(animationSteps, Is::LessThanOrEqualToZero);
+  WARNIF(animationDelayMS, Is::LessThanZero);
 }
 
 
 inline void CheckConfigValues_Debug()
 {
-  WARNIF(debug_blankVal, CheckType::LESSTHANZERO);
+  WARNIF(debug_blankVal, Is::LessThanZero);
 
-  if (debug_noiseType < NoiseType::NONE) { WARN(debug_noiseType, CheckType::NOTANOPTION); }
+  if (debug_noiseType < NoiseType::NONE) { WARN(debug_noiseType, Is::NotAnOption); }
 
-  WARNIF(debug_blankRegionModifier,CheckType::LESSTHANZERO);
+  WARNIF(debug_blankRegionModifier,Is::LessThanZero);
 
-  WARNIF(debug_reportTimeMS, CheckType::LESSTHANZERO);
+  WARNIF(debug_reportTimeMS, Is::LessThanZero);
 
-  WARNIF(debug_scriptAnimation_cols, CheckType::LESSTHANOREQUALTOZERO);
-  WARNIF(debug_scriptAnimation_cols, CheckType::LESSTHANOREQUALTOZERO);
+  WARNIF(debug_scriptAnimation_cols, Is::LessThanOrEqualToZero);
+  WARNIF(debug_scriptAnimation_cols, Is::LessThanOrEqualToZero);
 }
 
 
