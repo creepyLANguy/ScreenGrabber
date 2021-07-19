6dfcf7af947e1db41c45d6b9d839f071606feba8
Altamish Mahomed <gambit318@gmail.com>
2021-07-19 13:47:23 +0200

    Update comments on sequence of execution.

--- ScreenGrabber/ScreenGrabber/Source.cpp
+++ ScreenGrabber/ScreenGrabber/Source.cpp
@@ -18,31 +18,31 @@ int main(const int argc, char** argv)
 {
   SetConsoleTitle(kApplicationName);
 
-  //1.
+  //Step 1
   InitConfigVariables();
 
-  //2.
+  //Step 2
   ProcessCommandLine(argc, argv);
 
-  //3.
+  //Step 3
   SetupSockets(tempSockets, sockets);
 
-  //4.
+  //Step 4
   RunBlackoutBroadcast();
   
-  //5.
+  //Step 5
   if (resetServer == true)
   {
     RunServerConfigBroadcast();
   }
 
-  //6.
+  //Step 6
   if (mode != Mode::CONFIG && runFunc)
   {
     runFunc();
   }
 
-  //7.
+  //Step 7
   CleanUpDeviceContextStuffs();
 
   return 0;
