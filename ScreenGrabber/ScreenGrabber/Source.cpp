95d5457cd8cd8c19d60dd042b3d31ca39ffda1f4
Altamish Mahomed <gambit318@gmail.com>
2021-07-19 13:44:51 +0200

    Update comments on sequence of execution.

--- ScreenGrabber/ScreenGrabber/Source.cpp
+++ ScreenGrabber/ScreenGrabber/Source.cpp
@@ -10,32 +10,39 @@ MAINTAIN THIS SEQUENCE:
 3. Set up your sockets as per config.
 4. Send blackout packet to reset all LEDs. This step is kinda optional.
 5. Send server config packet which contains led count and physical brightness. Also kinda optional but is a good idea.
-6. If you do step 4, you'll need to wait a certain amount of time to give the servers a chance to reconfigure themselves.
-7. Run the corresponding function for the configured capture mode. May loop indefinitely.
-8. Clean up anything that needs cleaning up. May not be reached in some modes.
+5.1. If you do step 4, you'll need to wait a certain amount of time to give the servers a chance to reconfigure themselves.
+6. Run the corresponding function for the configured capture mode. May loop indefinitely.
+7. Clean up anything that needs cleaning up. May not be reached in some modes.
 */
 int main(const int argc, char** argv)
 {
   SetConsoleTitle(kApplicationName);
 
+  //1.
   InitConfigVariables();
 
+  //2.
   ProcessCommandLine(argc, argv);
 
+  //3.
   SetupSockets(tempSockets, sockets);
 
+  //4.
   RunBlackoutBroadcast();
-
+  
+  //5.
   if (resetServer == true)
   {
     RunServerConfigBroadcast();
   }
 
+  //6.
   if (mode != Mode::CONFIG && runFunc)
   {
     runFunc();
   }
 
+  //7.
   CleanUpDeviceContextStuffs();
 
   return 0;
