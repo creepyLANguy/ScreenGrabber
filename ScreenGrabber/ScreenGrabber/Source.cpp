41502c13a7a67948fffb80ac28851797605bc373
Altamish Mahomed <gambit318@gmail.com>
2021-07-23 12:16:35 +0200

    Fixed comment.

--- ScreenGrabber/ScreenGrabber/Source.cpp
+++ ScreenGrabber/ScreenGrabber/Source.cpp
@@ -10,7 +10,7 @@ MAINTAIN THIS SEQUENCE:
 3. Set up your sockets as per config.
 4. Send blackout packet to reset all LEDs. This step is kinda optional.
 5. Send server config packet which contains led count and physical brightness. Also kinda optional but is a good idea.
-5.1. If you do step 4, you'll need to wait a certain amount of time to give the servers a chance to reconfigure themselves.
+5.1. If you sent a config packet, you'll need to wait a certain amount of time to give the servers a chance to reconfigure themselves.
 6. Run the corresponding function for the configured capture mode. May loop indefinitely.
 7. Clean up anything that needs cleaning up. May not be reached in some modes.
 */
